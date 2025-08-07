5666d7869f459bae77aa57866851823aef54e562
khushih8900 <khushih191@gmail.com>
2025-08-07 12:09:13 +0600

    add sponsor section

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 
 
-  const menuToggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('.nav');
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
 
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+
+
+
+
+
 
