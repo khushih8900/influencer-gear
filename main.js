5e8497fb7f30083af9c615d9589357da3348a031
khushih8900 <khushih191@gmail.com>
2025-08-07 19:30:44 +0600

    add products and footer section

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
 
+/ products see more click/
 
+const btn = document.getElementById('seeMoreBtn');
+const grid = document.querySelector('.product-grid');
 
-
-
-
-
+btn.addEventListener('click', () => {
+  grid.classList.toggle('expanded');
+  btn.textContent = grid.classList.contains('expanded') ? 'See Less ' : 'See More';
+});
