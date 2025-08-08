c3a9d5d6f9c68285bb7cfaf876fc03584b14932c
khushih8900 <khushih191@gmail.com>
2025-08-08 10:40:18 +0600

    add click js on sources

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ btn.addEventListener('click', () => {
   grid.classList.toggle('expanded');
   btn.textContent = grid.classList.contains('expanded') ? 'See Less ' : 'See More';
 });
+/ product card  /
+const productCards = document.querySelectorAll('.productCard');
+const sources = document.querySelector('.sources');
+
+function activeLink() {
+  // Remove 'active' class from all cards
+  productCards.forEach((item) => item.classList.remove('active'));
+
+  // Add 'active' class to clicked card
+  this.classList.add('active');
+
+  // Move `.sources` element into the clicked `.productCard`
+  this.prepend(sources);
+}
+
+// Add event listener to each product card
+productCards.forEach((item) => {
+  item.addEventListener('click', activeLink);
+});
